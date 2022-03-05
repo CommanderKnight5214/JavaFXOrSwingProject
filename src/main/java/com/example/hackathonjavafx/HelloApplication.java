829f3af15cb83a518e88325d59ebf186f911a84d
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2022-03-05 16:54:34 -0600

    updates for clock

--- src/main/java/com/example/hackathonjavafx/HelloApplication.java
+++ src/main/java/com/example/hackathonjavafx/HelloApplication.java
@@ -1,13 +1,29 @@
 package com.example.hackathonjavafx;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private Button btnTest;
+
+    @FXML
+    private Label TimeLabel;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -16,7 +32,9 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.setScene(scene);
         stage.show();
+
     }
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/com/example/hackathonjavafx/HelloController.java
+++ src/main/java/com/example/hackathonjavafx/HelloController.java
@@ -1,11 +1,43 @@
 package com.example.hackathonjavafx;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
+import org.w3c.dom.events.MouseEvent;
+
+import java.time.Clock;
+import java.util.Timer;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class HelloController {
+    Timer timer = new Timer();
+    private Clock clock;
     @FXML
     private Label welcomeText;
 
+    @FXML
+    private Button btnTest;
 
+    @FXML
+    private Label TimeLabel;
+
+    @FXML
+    protected void handleButtonAction(ActionEvent e){
+        btnTest.setText("Hello World");
+    }
+
+    
+
+    public void timerMode(Event event) {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            TimeLabel.setText(dtf.format(now));
+    }
 }
