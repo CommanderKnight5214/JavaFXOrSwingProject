8d3a4a946399373cddc476606829e37838025820
CUWSulli <sean.sullivan1@cuw.edu>
2022-03-05 16:05:44 -0600

    Please dont break

--- src/main/java/com/example/hackathonjavafx/HelloApplication.java
+++ src/main/java/com/example/hackathonjavafx/HelloApplication.java
@@ -1,13 +1,16 @@
 package com.example.hackathonjavafx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.control.Button;
+import javafx.event.EventHandler;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application implements EventHandler<ActionEvent> {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -16,6 +19,14 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.setScene(scene);
         stage.show();
+        //start_button = new Button();
+        //start_button.setOnAction(this);
+
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/hackathonjavafx/HelloController.java
+++ src/main/java/com/example/hackathonjavafx/HelloController.java
@@ -1,11 +1,31 @@
 package com.example.hackathonjavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+    @FXML
+    private Button start_button;
 
+    @FXML
+    private Label hour;
+
+    @FXML
+    void buttonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    protected void handleButtonAction(ActionEvent e) {
+        start_button.setText("Hello World");
+    }
+    @FXML
+    protected void somethingButton(ActionEvent e) {
+        hour.setText("20");
+    }
 }
